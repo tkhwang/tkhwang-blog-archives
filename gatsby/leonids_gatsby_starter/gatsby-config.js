aae36870462240560410a07eb49842b9333007d0
Taekeun Hwang <tkhwang@gmail.com>
2022-04-02 14:16:52 +0900

    feat: import current gatsby blog

---
+++ gatsby/leonids_gatsby_starter/gatsby-config.js
@@ -0,0 +1,97 @@
+module.exports = {
+  pathPrefix: "/",
+  siteMetadata: {
+    title: `tkhwang.me`,
+    author: {
+      name: `@tkhwang`,
+      summary: ` 🌱 I want to DEVELOP something FUN and USEFUL.`,
+    },
+    description: `rtfm().then((data) => practice(data)).then((knowledge) => develop(knowledge))`,
+    siteUrl: `https://tkhwang.me`,
+    social: {
+      twitter: `tkhwang`,
+    },
+    defaultImage: "images/bg.jpeg",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-62251910-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ryz`,
+        short_name: `Ryz`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`,
+    "gatsby-plugin-dark-mode",
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          "IBM Plex Sans KR",
+          "Do Hyeon",
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+      },
+    },
+  ],
+}
